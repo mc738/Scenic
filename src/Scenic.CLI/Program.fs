b317a67d2843b57b81538bb3f6262be73a189835
Max <me@mclifford.dev>
2026-09-01 21:33:59 +0100

    chore(core): Work on scenic core

--- src/Scenic.CLI/Program.fs
+++ src/Scenic.CLI/Program.fs
@@ -1,2 +1,8 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿
+
+open FsToolbox.GLTF
+
+let vl = GLTFLoader.loadModel @"C:\Users\mclif\Projects\Blender\prims\new_cube.gltf"
+
+// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
---
+++ src/Scenic.Core/Components/ModelComponent.fs
@@ -0,0 +1,68 @@
+﻿namespace Scene.Core.Components
+
+open System
+open FsToolbox.GLTF
+open FsToolbox.GameDevelopment.Geometry.Types
+open FsToolbox.OpenGL.Materials
+open CommonResourceFormats.AssetStore.Core.Domain
+
+type ModelComponent() =
+    
+    let mutable material: OpenGLMaterial option = None
+    
+    let mutable model: Model3D option = None
+    
+    static member ComponentTypeName = "scenic:model"
+    
+    
+    static member Load(comp: Component) =
+        let materialAsset =
+            comp.Assets |> Seq.tryFind (fun ca -> ca.Asset.AssetType.Equals("opengl:material", StringComparison.OrdinalIgnoreCase))
+        
+        let modelAsset =
+            comp.Assets
+            |> Seq.tryFind (fun ca -> ca.Asset.AssetType.Equals("crf:model", StringComparison.OrdinalIgnoreCase))
+            |> Option.bind (fun ma ->
+                ma.Asset.Resources
+                |> Seq.tryFind (fun r ->
+                    match r.FileType with
+                    | "gltf" -> true
+                    //| "model" -> true
+                    | _ -> false))
+            |> Option.map (fun ma ->
+                match ma.FileType with
+                | "gltf" -> GLTFLoader.loadModel
+                
+                
+                
+                ()
+                
+                )
+        
+        
+        let modelComp = ModelComponent()
+        
+        //materialAsset |> Option.iter (fun ma -> modelComp.SetMaterial ma)
+        modelAsset |> Option.iter (fun ma -> modelComp.SetModel ma)
+        
+        modelComp
+    
+    static member Deserialize(jsonString: string) =
+        // Build from json.
+        
+        
+        
+        ()
+    
+    member _.SetModel(newModel: Model3D) =
+        model <- Some newModel
+        
+    member _.SetMaterial(newMaterial: OpenGLMaterial) =
+        material <- Some newMaterial
+        
+    
+    member _.Serialize() =
+        
+        ""
+        
+        
